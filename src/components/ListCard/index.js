c9c69d2e8f0f7f9ffa23e96327e837883b99399b
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-06-13 15:02:14 +0100

    pushit

---
+++ src/components/ListCard/index.js
@@ -0,0 +1,88 @@
+// @mui material components
+import { Divider } from "@mui/material";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import PropTypes from "prop-types";
+
+ListCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  bigTitle: PropTypes.any.isRequired,
+  items: PropTypes.array.isRequired,
+  item: PropTypes.any,
+  itemTitle: PropTypes.any,
+  link: PropTypes.any,
+};
+
+function ListCard({ title, bigTitle, items }) {
+  return (
+    <Card>
+      <MDBox pt={3} px={2}>
+        <MDTypography pl={2} variant="h5" fontWeight="normal">
+          {title}
+        </MDTypography>
+      </MDBox>
+      <MDBox pt={1} px={2}>
+        <MDTypography pl={2} variant="h1" fontWeight="bold">
+          {bigTitle}
+        </MDTypography>
+      </MDBox>
+      <MDBox pt={1} pb={2} px={2}>
+        <MDBox
+          component="ul"
+          display="flex"
+          style={{ listStyleType: "none" }}
+          flexDirection="column"
+          p={0}
+          m={0}
+        >
+          {Array.isArray(items) ? (
+            items.map((item) => {
+              return (
+                <>
+                  <MDBox
+                    component="a"
+                    href={item.link}
+                    rel="noreferrer"
+                    display="flex"
+                    pt={0}
+                    style={{ justifyContent: "space-between" }}
+                  >
+                    <MDTypography pl={2} variant="subtitle2" fontWeight="normal">
+                      {item.itemTitle}
+                    </MDTypography>
+                    <MDTypography pr={2} variant="subtitle2" fontWeight="bold">
+                      {item.item}
+                    </MDTypography>
+                  </MDBox>
+                  <Divider />
+                </>
+              );
+            })
+          ) : (
+            <MDBox
+              component="a"
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              display="flex"
+              pt={1}
+              style={{ justifyContent: "space-between" }}
+            >
+              <MDTypography pl={2} variant="subtitle2" fontWeight="normal">
+                {itemTitle}
+              </MDTypography>
+              <MDTypography pr={2} variant="subtitle2" fontWeight="bold">
+                {item}
+              </MDTypography>
+            </MDBox>
+          )}
+        </MDBox>
+      </MDBox>
+    </Card>
+  );
+}
+
+export default ListCard;
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,19 +1,75 @@
 // @mui material components
 import Grid from "@mui/material/Grid";
 
-// Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+import ListCard from "components/ListCard";
+
+const Registration = [
+  { itemTitle: "Total Registration", item: 5, link: "/registration" },
+  { itemTitle: "Total Amount", item: 1900, link: "#" },
+  { itemTitle: "Due Amount", item: 1900, link: "#" },
+  { itemTitle: "Paid Amount", item: 0, link: "#" },
+];
+
+const Applications = [
+  { itemTitle: "Total Applications", item: 20, link: "/application" },
+  { itemTitle: "Draft", item: 20, link: "#" },
+  { itemTitle: "Submitted", item: 0, link: "#" },
+  { itemTitle: "Approved", item: 0, link: "#" },
+  { itemTitle: "Rejected", item: 0, link: "#" },
+  { itemTitle: "Judging", item: 0, link: "#" },
+  { itemTitle: "Publice Voting", item: 0, link: "#" },
+];
+
 function Dashboard() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <MDBox py={3}>
-        <p>Hello</p>
-      </MDBox>
+      <Grid container spacing={1} style={{ marginTop: "2rem" }}>
+        <Grid item xs={12} md={6} xl={3}>
+          <ComplexStatisticsCard
+            color="info"
+            icon="add_home_work"
+            count="Registration"
+            percentage={{ color: "info", label: "In Active" }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} xl={3}>
+          <ComplexStatisticsCard
+            color="info"
+            icon="app_registration"
+            count="Application"
+            percentage={{ color: "info", label: "In Active" }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} xl={3}>
+          <ComplexStatisticsCard
+            color="warning"
+            icon="gavel"
+            count="Judging"
+            percentage={{ color: "warning", label: "In Progress" }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} xl={3}>
+          <ComplexStatisticsCard
+            color="warning"
+            icon="how_to_vote"
+            count="Public Voting"
+            percentage={{ color: "warning", label: "In Progress" }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={1} style={{ marginTop: "2rem" }}>
+        <Grid item xs={12} md={6}>
+          <ListCard title="Registration" bigTitle="5" items={Registration} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ListCard title="Applications" bigTitle="20" items={Applications} />
+        </Grid>
+      </Grid>
     </DashboardLayout>
   );
 }
