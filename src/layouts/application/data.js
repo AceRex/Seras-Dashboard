d955ac5766f8d669a807c4fb32584cba2bc231f0
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-06-13 17:13:33 +0100

    pushing it

--- src/layouts/application/data.js
+++ src/layouts/application/data.js
@@ -5,30 +5,28 @@ import MDTypography from "components/MDTypography";
 
 export const columns = [
   { Header: "ID", accessor: "id", width: "10%", align: "center" },
-  { Header: "Date", accessor: "date", width: "10%", align: "center" },
+  { Header: "Last edited", accessor: "last_edited", width: "10%", align: "center" },
+  { Header: "Registrant", accessor: "registrant", width: "20%", align: "center" },
   { Header: "Name", accessor: "name", width: "20%", align: "center" },
-  { Header: "Company", accessor: "company", width: "20%", align: "center" },
   { Header: "Category", accessor: "category", width: "20%", align: "center" },
-  { Header: "Applications", accessor: "applications", width: "5%", align: "center" },
-  { Header: "Total USD", accessor: "total_usd", width: "5%", align: "center" },
   { Header: "Status", accessor: "status", width: "5%", align: "center" },
+  { Header: "Result", accessor: "result", width: "5%", align: "center" },
+  { Header: "Rounds", accessor: "rounds", width: "5%", align: "center" },
+  { Header: "Tags", accessor: "tags", width: "1%", align: "center" },
   { Header: "Action", accessor: "action", width: "1%", align: "center" },
 ];
 
 export const rows = [
   {
     id: "090922282",
-    date: "12/03/2020",
-    name: "Are Oluwasegun Johnson",
-    company: "something",
+    last_edited: "10 Jun",
+    registrant: "Are Oluwasegun Johnson",
+    name: "Oluwasegun",
     category: "Best Company",
-    applications: "4",
-    total_usd: (
-      <MDTypography variant="caption" color="text" fontWeight="medium">
-        $14,000
-      </MDTypography>
-    ),
     status: <MDBadge badgeContent="not paid" color="warning" />,
+    result: "77",
+    rounds: "4",
+    tags: "something",
     action: (
       <IconButton
         size="small"
@@ -48,18 +46,15 @@ export const rows = [
     ),
   },
   {
-    id: "090922290",
-    date: "12/03/2020",
-    name: "Are Oluwasegun Johnson",
-    company: "something",
-    category: "CEO of the year",
-    applications: "4",
-    total_usd: (
-      <MDTypography variant="caption" color="text" fontWeight="medium">
-        $14,000
-      </MDTypography>
-    ),
-    status: <MDBadge badgeContent="paid" color="success" />,
+    id: "090922282",
+    last_edited: "10 Jun",
+    registrant: "Are Oluwasegun Johnson",
+    name: "Oluwasegun",
+    category: "Best Company",
+    status: <MDBadge badgeContent="not paid" color="warning" />,
+    result: "77",
+    rounds: "4",
+    tags: "something",
     action: (
       <IconButton
         size="small"
--- src/layouts/application/index.js
+++ src/layouts/application/index.js
@@ -7,13 +7,41 @@ import MDBox from "components/MDBox";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import { columns, rows } from "./data";
+import MDButton from "components/MDButton";
+import { Card, Icon } from "@mui/material";
+import DataTable from "examples/Tables/DataTable";
+
 function Application() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <MDBox py={3}>
-        <p>Hello</p>
-      </MDBox>
+      <Grid container spacing={6} pt={4}>
+        <Grid item xs={12}>
+          <Card>
+            <MDBox display="flex" pl={4} pt={3}>
+              <MDBox pr={1}>
+                <MDButton variant="gradient" color="warning">
+                  <Icon>add</Icon>&nbsp; Add Registration
+                </MDButton>
+              </MDBox>
+              <MDBox>
+                <MDButton variant="gradient" color="dark">
+                  Actions &nbsp;<Icon>keyboard_arrow_down</Icon>
+                </MDButton>
+              </MDBox>
+            </MDBox>
+            <MDBox pt={3}>
+              <DataTable
+                table={{ columns, rows }}
+                isSorted={false}
+                entriesPerPage={false}
+                showTotalEntries={false}
+              />
+            </MDBox>
+          </Card>
+        </Grid>
+      </Grid>
     </DashboardLayout>
   );
 }
