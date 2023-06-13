30e58baf22b28708721d5ce4a937ada91d43b813
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-06-13 17:01:48 +0100

    pushing

--- src/layouts/registration/index.js
+++ src/layouts/registration/index.js
@@ -1,19 +1,46 @@
-// @mui material components
-import Grid from "@mui/material/Grid";
-
 // Material Dashboard 2 React components
+import { Card, Grid } from "@mui/material";
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
 
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import DataTable from "examples/Tables/DataTable";
+import { columns, rows } from "./data";
+import MDButton from "components/MDButton";
+import Icon from "@mui/material/Icon";
+
 function Registration() {
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <MDBox py={3}>
-        <p>Hello</p>
-      </MDBox>
+      <Grid container spacing={6} pt={4}>
+        <Grid item xs={12}>
+          <Card>
+            <MDBox display="flex" pl={4} pt={3}>
+              <MDBox pr={1}>
+                <MDButton variant="gradient" color="warning">
+                  <Icon>add</Icon>&nbsp; Add Registration
+                </MDButton>
+              </MDBox>
+              <MDBox>
+                <MDButton variant="gradient" color="dark">
+                  Actions &nbsp;<Icon>keyboard_arrow_down</Icon>
+                </MDButton>
+              </MDBox>
+            </MDBox>
+            <MDBox pt={3}>
+              <DataTable
+                table={{ columns, rows }}
+                isSorted={false}
+                entriesPerPage={false}
+                showTotalEntries={false}
+              />
+            </MDBox>
+          </Card>
+        </Grid>
+      </Grid>
     </DashboardLayout>
   );
 }
